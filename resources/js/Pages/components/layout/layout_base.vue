ead85e256dedfa6822be5eea601eb32774d438f3
duds <duadsal@gmail.com>
2024-09-04 15:37:36 -0300

    develop | Ajuste form e sidebar

--- resources/js/Pages/components/layout/layout_base.vue
+++ resources/js/Pages/components/layout/layout_base.vue
@@ -1,7 +1,7 @@
 <template>
   <side_bar @menu-fechado="handleMenuFechado" @menu-aberto="handleMenuAberto" />
 
-  <div :class="sidebarClass">
+  <div :class="sidebarClass" class="transition-all duration-500 ease-in-out">
     <div class="bg-gray-200 rounded-lg p-4">
       <slot></slot>
     </div>
@@ -17,13 +17,11 @@ const sidebarClass = ref('w-full min-h-screen pt-10 px-4 sm:px-6 md:px-8 lg:ps-7
 
 // Função para lidar com o evento de menu fechado
 const handleMenuFechado = () => {
-  console.log('Menu fechado event received');
   sidebarClass.value = 'w-full min-h-screen pt-10 px-4 sm:px-6 md:px-8 lg:ps-28 bg-gray-50';
 };
 
 // Função para lidar com o evento de menu aberto
 const handleMenuAberto = () => {
-  console.log('Menu aberto event received');
   sidebarClass.value = 'w-full min-h-screen pt-10 px-4 sm:px-6 md:px-8 lg:ps-72 bg-gray-50';
 };
 </script>
